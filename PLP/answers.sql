ed6e833eb1500ffdb3952ff0db5fa1d0959fefd8
ndush <mdamarisnduku@gmail.com>
2025-05-21 10:41:29 +0300

    add

--- PLP/answers.sql
+++ PLP/answers.sql
@@ -41,3 +41,40 @@ GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
 -- Question 4 🔐
 -- Change the password for user 'bob' to a new secure value
 ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
+
+-- Question 1 🧑‍💼
+-- Get firstName, lastName, email, and officeCode of all employees using INNER JOIN with offices
+SELECT
+    e.firstName,
+    e.lastName,
+    e.email,
+    e.officeCode
+FROM
+    employees e
+INNER JOIN
+    offices o ON e.officeCode = o.officeCode;
+
+-- Question 2 🛍️
+-- Get productName, productVendor, and productLine using LEFT JOIN with productlines
+SELECT
+    p.productName,
+    p.productVendor,
+    p.productLine
+FROM
+    products p
+LEFT JOIN
+    productlines pl ON p.productLine = pl.productLine;
+
+-- Question 3 📦
+-- Retrieve orderDate, shippedDate, status, and customerNumber using RIGHT JOIN with customers and limit to 10 orders
+SELECT
+    o.orderDate,
+    o.shippedDate,
+    o.status,
+    o.customerNumber
+FROM
+    customers c
+RIGHT JOIN
+    orders o ON c.customerNumber = o.customerNumber
+LIMIT 10;
